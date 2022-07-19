3d8e9d72bd43eed10ec083671b8a40a4c5a64077
Evgeny <evgeny.vereshchagin@alemira.com>
2022-07-19 11:16:48 +0800

    a

--- renderer.js
+++ renderer.js
@@ -41,19 +41,15 @@ out.addEventListener('click', async (evt) => {
     }
 })
 
-// Get the modal
-var modal = document.getElementById("searchModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("searchButton");
+var modal = document.getElementById("searchModal");
+var searchBtn = document.getElementById("searchButton");
 var search = document.getElementById("search");
 var searchVal = document.getElementById("searchVal")
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+
+searchBtn.onclick = function() {
   modal.style.display = "block";
 }
 search.onclick = function() {
@@ -61,12 +57,10 @@ search.onclick = function() {
     window.find(searchVal.value);
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
